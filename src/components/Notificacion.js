908e2658b740d8d71be1c4c0b8c08b6131153fb6
rick2716 <ricardo.alvarez@uao.edu.co>
2022-04-10 14:33:20 -0500

    popup y notificacion components

---
+++ src/components/Notificacion.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Notificacion = () => {
+    return (
+        <div className='notificacion-container'>
+            <p>Ya ingresaste esta letra</p>
+        </div>
+    );
+};
+
+export default Popup;
---
+++ src/components/Popup.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Popup = () => {
+    return (
+        <div className='popup-container'>
+            <div className='popup'>
+                <h2></h2>
+                <h3></h3>
+            </div>
+        </div>
+    );
+};
+
+export default Popup;
