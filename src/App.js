0327132ea1b874129a5313eb6fb578e864d55e8b
rick2716 <ricardo.alvarez@uao.edu.co>
2022-04-10 15:00:53 -0500

    useState iniciales

--- src/App.js
+++ src/App.js
@@ -4,11 +4,16 @@ import Header from './components/Header';
 import Figura from './components/Figura';
 import LetrasErradas from './components/LetrasErradas';
 import Palabra from './components/Palabra';
+import { useState } from 'react';
 
 const palabras = ['application', 'programming', 'interface', 'wizard'];
 let palabraElegida = words[Math.floor(Math.random() * words.length)];
 
 function App() {
+  const [playable, setPlayable] = useState(true);
+  const [letrasCorrectas, setLetrasCorrectas] = useState([]);
+  const [letrasIncorrectas, setLetrasIncorrectas] = useState([]);
+
   return (
     <div className="App">
       <Header />
